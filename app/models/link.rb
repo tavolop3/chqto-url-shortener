e63c96bdc98ca07f5f7cd0a79af6841895c34044
tavo <tavo.lop33@gmail.com>
2023-12-06 02:00:46 -0300

    agrego encriptacion para la contraseña del link

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,6 +3,7 @@ class Link < ApplicationRecord
   has_many :accesses, dependent: :destroy, counter_cache: true
   validates :url, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp }
   validates :slug, presence: true, uniqueness: true
+  encrypts :password
 
   def check_password(password)
     password == self.password
--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,11 @@ module Chqto
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.i18n.default_locale = :es
+
     config.hosts << 'chq.to'
+
+    config.active_record.encryption.primary_key = ENV['ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY']
+    config.active_record.encryption.deterministic_key = ENV['ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY']
+    config.active_record.encryption.key_derivation_salt = ENV['ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT']
   end
 end
