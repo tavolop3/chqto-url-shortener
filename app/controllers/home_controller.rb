84952aea800a03904d7f57435e5f38c9e1d0caaa
tavo <tavo.lop33@gmail.com>
2023-12-10 22:22:08 -0300

    agrego estadisticas en home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,20 @@ class HomeController < ApplicationController
   def index
     return unless user_signed_in?
 
+    home_stats
     render 'index_authenticated'
+  end
+
+  private
+
+  def home_stats
+    user = current_user
+    links = user.links.includes(:accesses)
+    @total_accesses_count = links.sum(&:accesses_count)
+    top_links = links.sort_by { |link| -link.accesses_count }
+    @pie_chart_data = top_links.map do |link|
+      name = link.name.present? ? link.name : link.url
+      [name, link.accesses_count]
+    end.to_h
   end
 end
